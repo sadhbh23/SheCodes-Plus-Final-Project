4142617f61e67ea1c3cb4d05bc50bcfc7acc0973
sadhbh23 <127418826+sadhbh23@users.noreply.github.com>
2023-04-09 00:13:13 +0100

    Added Icon to Reflect Weather Data from API Call

--- src/script.js
+++ src/script.js
@@ -28,15 +28,22 @@ function displayData(response) {
   let descriptionElement = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
   tempElement.innerHTML = Math.round(response.data.main.temp);
   dateTimeElement.innerHTML = formatDate(response.data.dt * 1000);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+let city = "amsterdam";
 let apiKey = "74373fd9c9c828199b66c103f4039a7d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=amsterdam&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayData);
